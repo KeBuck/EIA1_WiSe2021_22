10c5dbe2382890e31f0083ae2076b45a81ea83ae
KeBuck <72626918+KeBuck@users.noreply.github.com>
2021-11-11 12:52:32 +0100

    Text + var

--- Aufgaben/Aufgabe-05/Infografik/script.ts
+++ Aufgaben/Aufgabe-05/Infografik/script.ts
@@ -28,11 +28,12 @@ var continentTotalNorthAmerica: number = Math.round((continent4_2018 *100 / tota
 var continentTotalAsia: number = Math.round((continent5_2018 *100 / totalContinent_2018) *100) / 100;
 var continentTotalAustralia: number = Math.round((continent6_2018 *100 / totalContinent_2018) *100) / 100;
 
-var Africa2008_2018: number = Math.round(100-(continent1_2018 *100 / continent1_2008));
-var SouthAmerica2008_2018: number = Math.round(100-(continent2_2018 *100 / continent2_2008));
-var Europe2008_2018: number = Math.round(100-(continent3_2018 *100 / continent3_2008));
-var NorthAmerica2008_2018: number = Math.round(100-(continent4_2018 *100 / continent4_2008));
-var Asia2008_2018: number = Math.round(100-(continent5_2018 *100 / continent5_2008));
+var Africa2008_2018: number = Math.round(100 - (continent1_2018 *100 / continent1_2008));
+var SouthAmerica2008_2018: number = Math.round(100 - (continent2_2018 *100 / continent2_2008));
+var Europe2008_2018: number = Math.round(100  -(continent3_2018 *100 / continent3_2008));
+var NorthAmerica2008_2018: number = Math.round(100 - (continent4_2018 *100 / continent4_2008));
+var Asia2008_2018: number = Math.round(100 - (continent5_2018 *100 / continent5_2008));
+var Austrilia2008_2018: number = Math.round(100 - (continent6_2018 *100 / continent6_2008));
 
 var continent1_2008_2018: number = Math.round(continent1_2018 - continent1_2008);
 var continent2_2008_2018: number = Math.round(continent2_2018 - continent2_2008);
@@ -40,3 +41,32 @@ var continent3_2008_2018: number = Math.round(continent3_2018 - continent3_2008)
 var continent4_2008_2018: number = Math.round(continent4_2018 - continent4_2008);
 var continent5_2008_2018: number = Math.round(continent5_2018 - continent5_2008);
 var continent6_2008_2018: number = Math.round(continent6_2018 - continent6_2008);
+
+console.log ("Die Emission von" + continent1 +  "ist:" + continent1_2018 + "kg CO2.");
+console.log ("Die Emission von" + continent2 +  "ist:" + continent2_2018 + "kg CO2.");
+console.log ("Die Emission von" + continent3 +  "ist:" + continent3_2018 + "kg CO2.");
+console.log ("Die Emission von" + continent4 +  "ist:" + continent4_2018 + "kg CO2.");
+console.log ("Die Emission von" + continent5 +  "ist:" + continent5_2018 + "kg CO2.");
+console.log ("Die Emission von" + continent6 +  "ist:" + continent6_2018 + "kg CO2.");
+
+console.log ("Relativ zur Gesamtemission der Welt verursacht" + continent1 + "damit" + continentTotalAfrica + "%.");
+console.log ("Relativ zur Gesamtemission der Welt verursacht" + continent2 + "damit" + continentTotalSouthAmerica + "%.");
+console.log ("Relativ zur Gesamtemission der Welt verursacht" + continent3 + "damit" + continentTotalEurope + "%.");
+console.log ("Relativ zur Gesamtemission der Welt verursacht" + continent4 + "damit" + continentTotalNorthAmerica + "%.");
+console.log ("Relativ zur Gesamtemission der Welt verursacht" + continent5 + "damit" + continentTotalAsia + "%.");
+console.log ("Relativ zur Gesamtemission der Welt verursacht" + continent6 + "damit" + continentTotalAustralia + "%.");
+
+console.log ("Für" + continent1 + "hat sich 2018 im Vergleich zu 2008 die Emission um" + Africa2008_2018 + "%.");
+console.log ("Für" + continent2 + "hat sich 2018 im Vergleich zu 2008 die Emission um" + SouthAmerica2008_2018 + "%.");
+console.log ("Für" + continent3 + "hat sich 2018 im Vergleich zu 2008 die Emission um" + Europe2008_2018 + "%.");
+console.log ("Für" + continent4 + "hat sich 2018 im Vergleich zu 2008 die Emission um" + NorthAmerica2008_2018 + "%.");
+console.log ("Für" + continent5 + "hat sich 2018 im Vergleich zu 2008 die Emission um" + Asia2008_2018 + "%.");
+console.log ("Für" + continent6 + "hat sich 2018 im Vergleich zu 2008 die Emission um" + Austrilia2008_2018 + "%.");
+
+
+console.log ("Für" + continent1 + "2018 im Vergleich zu 2008 das" + continent1_2008_2018 + "kg CO2.");
+console.log ("Für" + continent2 + "2018 im Vergleich zu 2008 das" + continent2_2008_2018 + "kg CO2.");
+console.log ("Für" + continent3 + "2018 im Vergleich zu 2008 das" + continent3_2008_2018 + "kg CO2.");
+console.log ("Für" + continent4 + "2018 im Vergleich zu 2008 das" + continent4_2008_2018 + "kg CO2.");
+console.log ("Für" + continent5 + "2018 im Vergleich zu 2008 das" + continent5_2008_2018 + "kg CO2.");
+console.log ("Für" + continent6 + "2018 im Vergleich zu 2008 das" + continent6_2008_2018 + "kg CO2.");

e6e34aa865b95011bbe82e577183d22b16ab4ed9
KeBuck <72626918+KeBuck@users.noreply.github.com>
2021-11-11 11:54:04 +0100

    var name und number 2008 / 2018

---
+++ Aufgaben/Aufgabe-05/Infografik/script.ts
@@ -0,0 +1,20 @@
+var continent1: string = "Africa";
+var continent2: string = "South America";
+var continent3: string = "Europe";
+var continent4: string = "North America";
+var continent5: string = "Asia";
+var continent6: string = "Australia";
+
+var continent1_2008: number = 1.208;
+var continent2_2008: number = 1.132.6;
+var continent3_2008: number = 4.965.7;
+var continent4_2008: number = 6.600.4;
+var continent5_2008: number = 12.954.7;
+var continent6_2008: number = 1.993;
+
+var continent1_2018: number = 1.235.5;
+var continent2_2018: number = 1.261.5;
+var continent3_2018: number = 4.209.3;
+var continent4_2018: number = 6.305.6;
+var continent5_2018: number = 16.274.1;
+var continent6_2018: number = 2.100.5;

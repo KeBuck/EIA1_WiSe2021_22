680803cb06c4acf89c90929d4ff811169180cd9f
KeBuck <72626918+KeBuck@users.noreply.github.com>
2021-11-12 16:33:56 +0100

    var number correction

--- Aufgaben/Aufgabe-05/Infografik/script.ts
+++ Aufgaben/Aufgabe-05/Infografik/script.ts
@@ -5,19 +5,19 @@ var continent4: string = "North America";
 var continent5: string = "Asia";
 var continent6: string = "Australia";
 
-var continent1_2008: number = 1.208;
-var continent2_2008: number = 1.132.6;
-var continent3_2008: number = 4.965.7;
-var continent4_2008: number = 6.600.4;
-var continent5_2008: number = 12.954.7;
-var continent6_2008: number = 1.993;
+var continent1_2008: number = 1208;
+var continent2_2008: number = 1132.6;
+var continent3_2008: number = 4965.7;
+var continent4_2008: number = 6600.4;
+var continent5_2008: number = 12954.7;
+var continent6_2008: number = 1993;
 
-var continent1_2018: number = 1.235.5;
-var continent2_2018: number = 1.261.5;
-var continent3_2018: number = 4.209.3;
-var continent4_2018: number = 6.305.6;
-var continent5_2018: number = 16.274.1;
-var continent6_2018: number = 2.100.5;
+var continent1_2018: number = 1235.5;
+var continent2_2018: number = 1261.5;
+var continent3_2018: number = 4209.3;
+var continent4_2018: number = 6305.6;
+var continent5_2018: number = 16274.1;
+var continent6_2018: number = 2100.5;
 
 var totalContinent_2018: number = Math.round(continent1_2018 + continent2_2018 + continent3_2018 + continent4_2018 + continent5_2018 + continent5_2018*100)/100;
 

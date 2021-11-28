8a012aaa076dde0a5e93418739d38f93d5c11253
KeBuck <72626918+KeBuck@users.noreply.github.com>
2021-11-28 01:11:17 +0100

    test button sound play

--- Aufgaben/Aufgabe-07/script.js
+++ Aufgaben/Aufgabe-07/script.js
@@ -1,10 +1,11 @@
-var sound = [new Audio('assets/A.mp3'), new Audio('assets/C.mp3'), new Audio('assets/F.mp3'), new Audio('assets/G.mp3'), new Audio('assets/hihat.mp3'), new Audio('assets/kick.mp3'), new Audio('assets/laugh-2.mp3'), new Audio('assets/snare.mp3')];
+//var sound:HTMLAudioElement [] = [new Audio('assets/A.mp3'), new Audio('assets/C.mp3'), new Audio('assets/F.mp3'), new Audio('assets/G.mp3'), new Audio('assets/hihat.mp3'), new Audio('assets/kick.mp3'), new Audio('assets/laugh-2.mp3'), new Audio('assets/snare.mp3')];
+var sound1 = new Audio('assets/A.mp3');
+//var beats: string = "sound1";
 var key = 0;
-function soundPlay() {
-    console.log(soundPlay[key]);
-    key++;
+function soundPlay(sound1) {
+    sound1.play();
 }
 window.addEventListener("load", function () {
-    document.querySelector(".buttonSound").addEventListener("click", soundPlay);
+    document.querySelector("#button1").addEventListener("click", soundPlay);
 });
 //# sourceMappingURL=script.js.map

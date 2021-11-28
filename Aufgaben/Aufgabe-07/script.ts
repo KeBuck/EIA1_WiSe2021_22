140319d0c86761c983728bab5aa8a58de31635cb
KeBuck <72626918+KeBuck@users.noreply.github.com>
2021-11-28 01:14:52 +0100

    test play sound array with key

--- Aufgaben/Aufgabe-07/script.ts
+++ Aufgaben/Aufgabe-07/script.ts
@@ -1,4 +1,4 @@
-//var sound:HTMLAudioElement [] = [new Audio('assets/A.mp3'), new Audio('assets/C.mp3'), new Audio('assets/F.mp3'), new Audio('assets/G.mp3'), new Audio('assets/hihat.mp3'), new Audio('assets/kick.mp3'), new Audio('assets/laugh-2.mp3'), new Audio('assets/snare.mp3')];
+var sound:HTMLAudioElement [] = [new Audio('assets/A.mp3'), new Audio('assets/C.mp3'), new Audio('assets/F.mp3'), new Audio('assets/G.mp3'), new Audio('assets/hihat.mp3'), new Audio('assets/kick.mp3'), new Audio('assets/laugh-2.mp3'), new Audio('assets/snare.mp3')];
 
 var sound1:HTMLAudioElement = new Audio ('assets/A.mp3');
 
@@ -7,8 +7,8 @@ var sound1:HTMLAudioElement = new Audio ('assets/A.mp3');
 
 var key: number = 0;
 
-function soundPlay (sound1) {
-    sound1.play ();
+function soundPlay () {
+    sound [key].play();
 }
 
 window.addEventListener("load", function () {

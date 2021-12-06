4afb237cc225e11cd6e35e44a9e54d44128f9080
KeBuck <72626918+KeBuck@users.noreply.github.com>
2021-12-06 20:39:07 +0100

    interface script

--- Aufgaben/Aufgabe-09/script_09.ts
+++ Aufgaben/Aufgabe-09/script_09.ts
@@ -1,13 +1,19 @@
 window.addEventListener("load", function(): void {
 
-//Variablen
-var toDoList: HTMLElement = document.querySelector(".to-do-list");
-var addButton: HTMLElement = document.getElementById("addButton");
-var toDoInput: HTMLElement = document.querySelector("#newTask");
-let zaehler: number = 0;
-let i: number;
+    //Variablen
+    var toDoList: HTMLElement = document.querySelector(".to-do-list");
+    var addButton: HTMLElement = document.getElementById("addButton");
+    var toDoInput: HTMLElement = document.querySelector("#newTask");
+    let zaehler: number = 0;
+    let i: number;
 
+    //Interface
+    interface ToDo {
+        checked: boolean;
+        text: string;
+    }
 
+    
 
 
 
